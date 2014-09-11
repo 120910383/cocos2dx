5eb0ad5f143ad37b827a8db10df5f033562f3828
congcong <120910383@qq.com>
2014-09-12 00:43:40 +0800

    cocos2d-x-3.2 for me initialization

---
+++ tests/cpp-empty-test/proj.android/jni/build.bat
@@ -0,0 +1,2 @@
+@echo off
+ndk-build -C D:/learn.mobile/core_include/include/cocos2dx/tests/cpp-empty-test/proj.android/jni -I D:\learn.mobile\core_include\include\cocos2dx\external\winrt-specific\angle\include NDK_MODULE_PATH=D:\learn.mobile\core_include\include\cocos2dx;D:\learn.mobile\core_include\include\cocos2dx\cocos;D:\learn.mobile\core_include\include\cocos2dx\external
