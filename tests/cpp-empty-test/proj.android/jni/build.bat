25f1393996e79a72b2944c9553ab8537b56a0bd3
congcong <120910383@qq.com>
2014-12-26 13:45:38 +0800

    update to cocos2d-x-3.3

--- tests/cpp-empty-test/proj.android/jni/build.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-ndk-build -C D:/learn.mobile/core_include/include/cocos2dx/tests/cpp-empty-test/proj.android/jni -I D:\learn.mobile\core_include\include\cocos2dx\external\winrt-specific\angle\include NDK_MODULE_PATH=D:\learn.mobile\core_include\include\cocos2dx;D:\learn.mobile\core_include\include\cocos2dx\cocos;D:\learn.mobile\core_include\include\cocos2dx\external
